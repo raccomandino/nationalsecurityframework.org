40f1edfb0f64f577ac137a611482e2ab154f0a80
Star1215 <cryptomanagerstar@gmail.com>
2025-02-15 05:06:13 +1000

    Feat: Mainnet deployment

--- polarpound/src/constants.jsx
+++ polarpound/src/constants.jsx
@@ -5,9 +5,9 @@ export const BLOCK_RATE_SECONDS = 3;
 
 export const PRICE_DECIMALS = 1000;
 
-export const PRESALE_START_TS = 1738904400;
+export const PRESALE_START_TS = 1739557776;
 
-export const PRESALE_END_TS = 1741237200
+export const PRESALE_END_TS = 1759190400
 
 export const EXCHANGE_API_KEY = "094eabac2dd874b917a0bfa73b98af1a"
 
@@ -19,6 +19,12 @@ export const TOKEN_DECIMALS = 6;
 export const TRX_DECIMALS = 6;
 
 // TRON Nile Test Network, Wallet3 is owner
-export const SALE_TOKEN = "TWaAez751Hes3qTiecVMbMQDmbrL36pHNu";
-export const TRON_PRESALE_ADDR = "TWG6ToMd9oCrCSRTiFeraV6drwxQPHeRTj"
+// export const SALE_TOKEN = "TWaAez751Hes3qTiecVMbMQDmbrL36pHNu";
+// export const TRON_PRESALE_ADDR = "TWG6ToMd9oCrCSRTiFeraV6drwxQPHeRTj"
+// export const SALE_OWNER_ADDR = "TS6L1nxayRM5ruDw519aLSYjp33akUmwiK"
+
+
+// TRON Nile Test Network, Wallet3 is owner
+export const SALE_TOKEN = "TMwvZXV8vp3UBNF9i9uGLapGprpgKpUqqn";
+export const TRON_PRESALE_ADDR = "TZFkiAfVXUmo2exLwB4Agn87b8s796jw9P"
 export const SALE_OWNER_ADDR = "TS6L1nxayRM5ruDw519aLSYjp33akUmwiK"
