b32684fe7f894e0a0925cd4e088fe79508c39db0
Aomi Borah <103031412+its-aomi@users.noreply.github.com>
2025-02-19 00:09:34 +0530

    Moved back to previous version

--- polarpound/src/pages/api/tronweb.ts
+++ polarpound/src/pages/api/tronweb.ts
@@ -1,18 +1,9 @@
 // @ts-ignore
 import TronWeb from 'tronweb';
-
-// Load environment variable
-const TRON_API_KEY = process.env.NEXT_PUBLIC_TRON_API_KEY;
-
-if (!TRON_API_KEY) {
-    console.error('TRON API key not found in environment variables');
-}
-
 export const tronWeb: any = new TronWeb({
     fullHost: 'https://api.trongrid.io',
-    headers: { "TRON-PRO-API-KEY": TRON_API_KEY }
+    headers: {"TRON-PRO-API-KEY": "4c0e97ce-672d-4f3d-ad90-3c0652eb5a7e"}
 });
-
 if (typeof window !== 'undefined') {
     (window as any).tronWeb1 = tronWeb;
 }
