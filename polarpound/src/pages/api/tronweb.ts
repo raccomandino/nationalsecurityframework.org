3ef85fe36fdacda70fb901c5a92944c0cb4da297
Aomi Borah <103031412+its-aomi@users.noreply.github.com>
2025-02-20 17:03:47 +0530

    Updated tron key

--- polarpound/src/pages/api/tronweb.ts
+++ polarpound/src/pages/api/tronweb.ts
@@ -2,7 +2,7 @@
 import TronWeb from 'tronweb';
 export const tronWeb: any = new TronWeb({
     fullHost: 'https://api.trongrid.io',
-    headers: {"TRON-PRO-API-KEY": "54f87f44-f43c-4658-a573-30d69b7b5ca2"}
+    headers: {"TRON-PRO-API-KEY": "d77ea808-3155-45f5-8392-85cbd53bbdc9"}
 });
 if (typeof window !== 'undefined') {
     (window as any).tronWeb1 = tronWeb;
